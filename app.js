83bfb62203c5209ca5a9f2d73dc1b03a6c51a092
Connor Davies <96823004+VenexCon@users.noreply.github.com>
2022-07-03 08:43:36 +0100

    <refactor> setTimeout() added to WinnerCheck()

--- app.js
+++ app.js
@@ -4,43 +4,43 @@
 const gridcontainer = document.querySelector(".gridcontainer");
 const buttons = document.querySelector(".buttonDiv");
 const resetBtn = document.getElementById("resetGame");
-let   currentArray = [0,1,2,3,4,5,6,7,8];
+let currentArray = [0,1,2,3,4,5,6,7,8];
 
 
-
+const clearArray = () => {return currentArray = [0,1,2,3,4,5,6,7,8];}
 
 
 //Display Controller , initalised ready for click events
 const DisplayController = (function () {
     
-    this.displayStatus = false;
+   displayStatus = false;
 
-        const ConstructDivs = () => {buttons.addEventListener("click", (e) => {
-            if(e.target.id === "startGame" && this.displayStatus === false){
-                for (let i = 0; i < 9; i++){let item = document.createElement("DIV");
-                    item.setAttribute("index", `${i}`);
-                        item.classList.add("cell");
-                            gridcontainer.appendChild(item);
-                                this.displayStatus = true;
-                        }
+    const ConstructDivs = () => {buttons.addEventListener("click", (e) => {
+        if(e.target.id === "startGame" && this.displayStatus === false){
+            for (let i = 0; i < 9; i++){let item = document.createElement("DIV");
+                item.setAttribute("index", `${i}`);
+                    item.classList.add("cell");
+                        gridcontainer.appendChild(item);
+                            displayStatus = true;
                     }
                 }
-            );
-        };
+            }
+        );
+    };
 
-        const BoardReset =() => {buttons.addEventListener("click", (e) => {
-                if(e.target.id === "resetGame") {
-                    const cells = Array.from(document.querySelectorAll(".cell"))
-                        cells.forEach(cell => cell.innerText = ``)
-                    };
-                }
-            ) 
-        };
+    const BoardReset =() => {buttons.addEventListener("click", (e) => {
+            if(e.target.id === "resetGame") {
+                const cells = Array.from(document.querySelectorAll(".cell"))
+                    cells.forEach(cell => cell.innerText = ``)
+                };
+            }
+        ) 
+    };
 
-        const manualReset = function () {
-            cells = document.querySelectorAll(".cell");
-            cells.forEach(cell => cell.innerText ="");
-        }
+    const manualReset = function () {
+        cells = document.querySelectorAll(".cell");
+        cells.forEach(cell => cell.innerText ="");
+    };
 
 
         //modular calls
@@ -49,18 +49,19 @@ const DisplayController = (function () {
         return {manualReset}
     });
 
-
+    //instantiate gameBoard
 const display= DisplayController();
 
-// Player Factory called during gamneBoard() function, two players currently with pre-set values.
+
+// Player Factory called during gameBoard() function, two players currently with pre-set values.
 const player = (name, marker) => {
     
     this.name = name;
     this.marker = marker;
     const getName = () => name;
-        
         return {name, marker}
 };
+
 
 
 //GameBoard controller, this controls the game logic and conditional elements.
@@ -70,10 +71,6 @@ const gameboard = (() => {
     const player1 = player("connor", "x");
     const player2 = player("Sona","o");
 
-    
-    // currently unused left in test version
-    
-    function clearArray () {currentArray =[0,1,2,3,4,5,6,7,8]}
 
     const togglePlayer = (() => {if (this.playersGo ==="player1"){
             return playersGo = "player2"
@@ -90,8 +87,8 @@ const gameboard = (() => {
                             return togglePlayer()
 
         } else if (playersGo ="player2"){
+            currentArray[e.target.getAttribute("index")] = player2.marker;
                e.target.innerText = (player2.marker)
-                currentArray[e.target.getAttribute("index")] = player2.marker;
                     winnerCheck(currentArray);
                             return togglePlayer();
                         }
@@ -101,26 +98,26 @@ const gameboard = (() => {
     )();
 
     // currently is comparing empty cells - 
-    const winnerCheck = ((array) => {if (array.length > 3){
+    const winnerCheck = ((array) => {
             if (array[0] === array[3] && array[3] === array[6]||
                 array[0] === array[1] && array[1] === array[2]||
                 array[0] === array[4] && array[4] === array[8]||
                 array[1] === array[4] && array[4] === array[7]||
                 array[2] === array[4] && array[4] === array[6]||
-                array[3] === array[4] && array[4] === array[6]||
+                array[3] === array[4] && array[4] === array[5]||
                 array[6] === array[7] && array[7] === array[8]
-                    ){console.log(`The winner is ${playersGo}`)
-                        clearArray();
-                        display.manualReset();
+                    ){console.log(`The winner is ${playersGo}, game will reset in 6 seconds`)
+                        setTimeout(clearArray,3000)
+                        setTimeout(display.manualReset, 3000)
                         console.log("clearArray Triggered")
                         return 
                          
                     }
             }
-        }
+        
     );
 
-    return {Selection,currentArray,clearArray};
+    return {Selection,};
 });
 
 //instantiate gameBoard Object 
